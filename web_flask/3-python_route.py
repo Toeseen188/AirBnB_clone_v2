05c3ea08b1224e34bc7c88974c1ec5b58595406d
Hammed Tosin <hammedtosin188@gmail.com>
2023-03-11 14:32:44 -0800

    routes

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+Start a server in flask and route to ('/hbnb')
+@app.route('/python/)
+@app.route('/python/<text>')
+"""
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    " index page "
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ route /hbnb """
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def text(text):
+    """ use var in route """
+    return 'C %s' % text.replace('_', ' ')
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text="is cool"):
+    """ route '/python/text' where default
+    text= is cool
+    """
+    return 'Python %s' % text.replace('_', ' ')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
