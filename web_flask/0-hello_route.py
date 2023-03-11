f3e65e21cde88abeb55687e48211b948a3818324
Hammed Tosin <hammedtosin188@gmail.com>
2023-03-11 13:39:40 -0800

    added routes and strictslashes false

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -7,7 +7,10 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', strict_slashes=False)
 def index():
     " index page "
     return 'Hello HBNB!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
