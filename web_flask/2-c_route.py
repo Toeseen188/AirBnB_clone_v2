3cf46ae09056a7a6cc0d6d592369a1a289d7c803
Hammed Tosin <hammedtosin188@gmail.com>
2023-03-11 14:14:10 -0800

    using variable_name in route

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Start a server in flask and route to ('/hbnb')
+@app.route('/c/<text>')
+"""
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    " index page "
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ route /hbnb """
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def text(text):
+    """ use var in route """
+    return 'C %s' % text.replace('_', ' ')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
