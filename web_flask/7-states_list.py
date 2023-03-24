e68bbf2b6b685e8ab0bb8fbca4aa31bfc3889b3a
Hammed Tosin <hammedtosin188@gmail.com>
2023-03-24 10:46:43 -0700

    re-do

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,48 +1,27 @@
 #!/usr/bin/python3
-"""
-Script that starts a Flask web application:
-Your web application must be listening on 0.0.0.0, port 5000
-You must use storage for fetching data from the storage engine
-(FileStorage or DBStorage) => from models import storage and storage.all(...)
-After each request you must remove the current SQLAlchemy Session:
-    Declare a method to handle @app.teardown_appcontext
-    Call in this method storage.close()
-Routes:
-    /states_list: display a HTML page: (inside the tag BODY)
-        H1 tag: “States”
-        UL tag: with the list of all State objects present in DBStorage
-        sorted by name (A->Z) tip
-            LI tag: description of one State: <state.id>: <B><state.name></B>
-You must use the option strict_slashes=False in your route definition
-"""
-from flask import Flask
-from flask import render_template
+""" Script that runs an app with Flask framework """
+from flask import Flask, render_template
 from models import storage
 from models.state import State
 
+
 app = Flask(__name__)
-app.url_map.strict_slashes = False
-
-
-@app.route("/states_list")
-def states_list():
-    """
-    Return a HTML page: (inside the tag BODY)
-        H1 tag: “States”
-        UL tag: with the list of all State objects present in DBStorage
-        sorted by name (A->Z)
-            LI tag: description of one State: <state.id>: <B><state.name></B>
-    """
-    states = storage.all(State)
-    return render_template("7-states_list.html", states=states)
 
 
 @app.teardown_appcontext
-def teardown(exc):
-    """
-    Close database or file storage
-    """
+def teardown_session(exception):
+    """ Teardown """
     storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def display_html():
+    """ Function called with /states_list route """
+    states = storage.all(State)
+    dict_to_html = {value.id: value.name for value in states.values()}
+    return render_template('7-states_list.html',
+                           Table="States",
+                           items=dict_to_html)
 
 
 if __name__ == "__main__":
